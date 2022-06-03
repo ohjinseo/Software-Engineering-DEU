775bd5a6548b14f24c477991a10da8c4f149caf1
ohjinseo <62508156+ohjinseo@users.noreply.github.com>
2022-06-03 13:56:17 +0900

    feat : .html 주소 매핑

--- server/src/main/java/com/sw9/swe/controller/IndexController.java
+++ server/src/main/java/com/sw9/swe/controller/IndexController.java
@@ -9,4 +9,29 @@ public class IndexController {
     public String login(){
         return "login";
     }
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/addSubject")
+    public String addSubject() {
+        return "addSubject";
+    }
+
+    @GetMapping("/addStudent")
+    public String addStudent() {
+        return "addStudent";
+    }
+
+    @GetMapping("/studentList")
+    public String studentList() {
+        return "studentList";
+    }
+
+    @GetMapping("/subjectList")
+    public String subjectList() {
+        return "subjectList";
+    }
 }
